c9bafe5c67d3ce979bb382fa5b8e22d0abd1649e
rajapandians <dev8@dev8s-MacBook-Pro.local>
2013-07-17 19:03:19 +0530

    Screen shot added

--- RJLabel/ViewController.m
+++ RJLabel/ViewController.m
@@ -22,7 +22,7 @@
     
     MSGLabel *lll = [[MSGLabel alloc] initWithFrame:CGRectMake(35, 50, 250, 0)];
     lll.layer.cornerRadius = 5;
-    [lll setText:@"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" spaceAroudTextInPixel:8];
+    [lll setText:@"Liusmod tempor incididunt ut labore et dolore magna aliqua.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" spaceAroudTextInPixel:8];
     
     [self.view addSubview:lll];
 }
